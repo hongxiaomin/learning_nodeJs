7b4047479287ce9f8db109c7e8883c944c430643
hongxiaomin <597898948@qq.com>
2017-09-28 17:28:46 +0800

    node的概念

---
+++ node/1.callback.js
@@ -0,0 +1,10 @@
+function say(callback){
+    setTimeout(function(){
+        callback();
+    },1000)
+}
+function print(){
+    console.log('hello world')
+}
+
+say(print);
---
+++ node/2.global.js
@@ -0,0 +1,3 @@
+console.log(global);
+console.log(__filename);//当前模块的文件绝对路径
+console.log(__dirname);//当前模块的所在的目录的绝对路径
